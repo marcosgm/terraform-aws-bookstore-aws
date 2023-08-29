fb42de9d314262776dbb6c2bbf6f7d10d3583e0d
Marc Garcia (Consultant) <marc.garcia-ext@mcn.gouv.qc.ca>
2023-08-29 10:47:51 -0400

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -14,12 +14,6 @@ variable "subnet_ids" {
   default = ["subnet-39311d51", "subnet-4cc3ba36", "subnet-6450aa3b"]
 }
 
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random value so name is unique in your Azure subscription."
-}
-
 variable "ecs_prefix" {
   type        = string
   description = "Prefix of the container group name that's combined with a random value so name is unique"
